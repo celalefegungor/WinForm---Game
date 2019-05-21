4a0315b96dabf1cb51aeb005f679bfa31e86d3ef
celalefegungor <41279562+celalefegungor@users.noreply.github.com>
2019-05-21 20:22:12 +0300

    Add files via upload

---
+++ DolarPatlat/DolarPatlat/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace DolarPatlat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pctDollar = new System.Windows.Forms.PictureBox();
+            this.tmrOyun = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tmrGeriSayim = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pctDollar)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pctDollar
+            // 
+            this.pctDollar.BackColor = System.Drawing.Color.Transparent;
+            this.pctDollar.Image = ((System.Drawing.Image)(resources.GetObject("pctDollar.Image")));
+            this.pctDollar.InitialImage = null;
+            this.pctDollar.Location = new System.Drawing.Point(536, 265);
+            this.pctDollar.Name = "pctDollar";
+            this.pctDollar.Size = new System.Drawing.Size(54, 66);
+            this.pctDollar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctDollar.TabIndex = 0;
+            this.pctDollar.TabStop = false;
+            this.pctDollar.Click += new System.EventHandler(this.pctDollar_Click);
+            // 
+            // tmrOyun
+            // 
+            this.tmrOyun.Enabled = true;
+            this.tmrOyun.Interval = 1000;
+            this.tmrOyun.Tick += new System.EventHandler(this.tmrOyun_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PUAN : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(319, 52);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MAX PUAN :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(668, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(406, 52);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "KALAN HAK : 10";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(-1, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1111, 113);
+            this.panel1.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(668, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 52);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "SÜRE ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label6.Location = new System.Drawing.Point(963, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 52);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(928, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 52);
+            this.label5.TabIndex = 5;
+            this.label5.Text = ":";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(856, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 52);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // tmrGeriSayim
+            // 
+            this.tmrGeriSayim.Enabled = true;
+            this.tmrGeriSayim.Interval = 1000;
+            this.tmrGeriSayim.Tick += new System.EventHandler(this.tmrGeriSayim_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.BackgroundImage = global::DolarPatlat.Properties.Resources.bank;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1107, 593);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pctDollar);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dolar Patlat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pctDollar)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctDollar;
+        private System.Windows.Forms.Timer tmrOyun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer tmrGeriSayim;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ DolarPatlat/DolarPatlat/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace DolarPatlat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+
+        }
+        System.Media.SoundPlayer ses = new System.Media.SoundPlayer();
+        Random rnd = new Random();
+        int puan = 0;
+        int maxpuan = 0;
+        int hak = 10;
+        int saniye = 60;
+        int dakika = 4;
+
+        private void tmrOyun_Tick(object sender, EventArgs e)
+        {
+            int xKonum = rnd.Next(50, 750);
+            int yKonum = rnd.Next(100, 350);
+            pctDollar.Location = new Point(xKonum, yKonum);
+            pctDollar.Image = Image.FromFile(@"E:\Örnekler\DolarPatlat\giphy.gif");
+            pctDollar.Size = new Size(50, 50);
+
+            if (xKonum % 10 == 0)
+            {
+                pctDollar.Image = Image.FromFile(@"E:\Örnekler\DolarPatlat\coin2.gif");
+                pctDollar.Size = new Size(50, 50);
+                tmrOyun.Interval = 700;
+            }
+            else if (xKonum % 10 == 1)
+            {
+                pctDollar.Image = Image.FromFile(@"E:\Örnekler\DolarPatlat\flame-gif-transparent-5.gif");
+                pctDollar.Size = new Size(80, 80);
+                tmrOyun.Interval = 1100;
+            }
+            else
+            {
+                tmrOyun.Interval = 1000;
+            }
+        }
+
+
+
+        private void pctDollar_Click(object sender, EventArgs e)
+        {
+
+            if (tmrOyun.Interval == 1000)
+            {
+                pctDollar.Image = Image.FromFile(@"E:\Örnekler\DolarPatlat\comic-boom-explosion-2-1017x1024.png");
+                pctDollar.Size = new Size(100, 100);
+                ++puan;
+                pctDollar.BackColor = Color.Transparent;
+                label1.Text = "PUAN : " + puan.ToString();
+                ses.SoundLocation = "Cash Register Cha Ching-SoundBible.com-184076484.wav";
+                ses.Play();
+            }
+
+            else if (tmrOyun.Interval == 700)
+            {
+                puan = puan + 10;
+                label1.Text = "PUAN : " + puan.ToString();
+                ses.SoundLocation = "Cash Register Cha Ching-SoundBible.com-184076484.wav";
+                ses.Play();
+            }
+            else if (tmrOyun.Interval == 1100)
+            {
+
+                puan = 0;
+                label1.Text = "PUAN : " + puan.ToString();
+                ses.SoundLocation = "Fire_Burning-JaBa-810606813.wav";
+                ses.Play();
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            --hak;
+            label3.Text = "KALAN HAK : " + hak.ToString();
+            if (hak == 0)
+            {
+
+                if (puan >= maxpuan)
+                {
+                    maxpuan = puan;
+                    label2.Text = "MAX PUAN : " + maxpuan.ToString();
+                }
+                else
+                {
+                    label2.Text = "MAX PUAN : " + maxpuan.ToString();
+                }
+                tmrGeriSayim.Enabled = false;
+                pctDollar.Visible = false;
+                ses.SoundLocation = "Sad_Trombone-Joe_Lamb-665429450.wav";
+                ses.Play();
+
+                DialogResult dialogResult = MessageBox.Show("Tekrar Oynamak İster Misiniz ?", "OYUN BİTTİ !", MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    puan = 0;
+                    hak = 10;
+                    label1.Text = "PUAN : " + puan.ToString();
+                    label3.Text = "KALAN HAK : " + hak.ToString();
+                    pctDollar.Visible = true;
+                    dakika = 4;
+                    saniye = 59;
+                    tmrGeriSayim.Enabled = true;
+
+
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    Environment.Exit(1);
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.TransparencyKey = Color.FromKnownColor(KnownColor.Control);
+            this.Update();
+        }
+
+        private void tmrGeriSayim_Tick(object sender, EventArgs e)
+        {
+            label4.Text = "0"+dakika.ToString();
+            saniye = saniye - 1;
+            label6.Text = saniye.ToString();
+            if (saniye<10)
+            {
+                label6.Text = "0" + saniye.ToString();
+                if (saniye==0)
+                {
+                    if (saniye == 0 && dakika == 0)
+                    {
+
+                        if (puan >= maxpuan)
+                        {
+                            maxpuan = puan;
+                            label2.Text = "MAX PUAN : " + maxpuan.ToString();
+                        }
+                        else
+                        {
+                            label2.Text = "MAX PUAN : " + maxpuan.ToString();
+                        }
+                        pctDollar.Visible = false;
+                        ses.SoundLocation = "Sad_Trombone-Joe_Lamb-665429450.wav";
+                        ses.Play();
+                        tmrGeriSayim.Enabled = false;
+                        DialogResult dialogResult = MessageBox.Show("Tekrar Oynamak İster Misiniz ?", "OYUN BİTTİ !", MessageBoxButtons.YesNo);
+
+                        if (dialogResult == DialogResult.Yes)
+                        {
+                            puan = 0;
+                            hak = 10;
+                            label1.Text = "PUAN : " + puan.ToString();
+                            label3.Text = "KALAN HAK : " + hak.ToString();
+                            dakika = 5;
+                            saniye = 60;
+                            
+                            pctDollar.Visible = true;
+                            tmrGeriSayim.Enabled = true;
+
+                        }
+                        else if (dialogResult == DialogResult.No)
+                        {
+                            Environment.Exit(1);
+                        }
+                    }
+                    dakika = dakika - 1;
+                    saniye = 59;
+                }
+            }
+           
+            
+        }
+    }
+}
